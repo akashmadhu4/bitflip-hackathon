c1bea8c84b3bb8f05b1899f1d7f67092529b3d2d
Akash Madhu <akashmadhu4@gmail.com>
2020-11-04 13:14:52 +0530

    started setting up discordbot

---
+++ discordbot.js
@@ -0,0 +1,8 @@
+const Discord=require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready',()=>{
+    console.log('Ready');
+});
+
+client.login('NzczNDQwNzM2MjIxMjAwNDA1.X6JQ0Q.5hUYyG8O82wrNBF_wKeNi5clJjs')
