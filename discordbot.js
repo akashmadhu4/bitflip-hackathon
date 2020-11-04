dff655e668a3dd8074b5857922e16df76d117f6b
Akash Madhu <akashmadhu4@gmail.com>
2020-11-04 13:28:26 +0530

    added .env file

--- discordbot.js
+++ discordbot.js
@@ -1,8 +1,9 @@
 const Discord=require('discord.js');
+require('dotenv').config();
 const client = new Discord.Client();
 
 client.once('ready',()=>{
     console.log('Ready');
 });
 
-client.login('NzczNDQwNzM2MjIxMjAwNDA1.X6JQ0Q.5hUYyG8O82wrNBF_wKeNi5clJjs')
+client.login(process.env.BOT_TOKEN);
